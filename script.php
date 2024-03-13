2d6a1c37905da117e70559836053290bfac1ad8e
Marcos Gomez <marcoslg.gomez@gmail.com>
2024-03-12 23:13:46 -0300

    arrays

---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+
+$arregloIndexado= ["manzana", "pera", "sandia", "durazno"];
+$arregloDeArreglos = [
+    ["Daniel", "Juan", "Pedro", "Maria"], //0
+    ["manzana", "pera", "sandia", "durazno"], //1
+    ["Youtube", "Udemy", "Coder House"], //2
+];
+
+print_r($arregloDeArreglos);
+?>
--- variables.php
+++ variables.php
@@ -1,4 +1,8 @@
 <?php
 
+$numero = 1;
+$boolean = true;
+$cadena = "cadena";
 
+echo $numero;
 ?>
